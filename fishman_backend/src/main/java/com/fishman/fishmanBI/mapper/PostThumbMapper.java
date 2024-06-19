2b10f5f2e9b59990c13ec7193b85706f68857b09
fishman <cy13692810010@163.com>
2024-06-19 20:58:57 +0800

    初始化

---
+++ fishman_backend/src/main/java/com/fishman/fishmanBI/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.fishman.fishmanBI.mapper;
+
+import com.fishman.fishmanBI.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author fishman
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ fishman_backend/src/main/java/com/fishman/fishmanBI/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.fishman.fishmanBI.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fishman.fishmanBI.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author fishman
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
